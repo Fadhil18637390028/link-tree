e0b6b8458e3951d62c964552f0aeb54a82c6da00
Fadhil18637390028 <junior11alfhanino@gmail.com>
2025-05-17 15:25:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// for loop)
+
+// 3 statemen
+
+// let nama = 'fadhil' //tipe string 
+// let usia = 14 //tipe integer number
+// let tinggibadan = 173.5 //tipe double float
+// let beratbadan
+// let pacar = 452343
+
+// beratbadan = 62
+
+// if(pacar == null){
+//    pacar = 'belum punya'
+// } else {
+//    pacar = 'sudah punya'
+// }
+
+// let saldoawal = 1000000
+// let saldotambahan = 6000000
+// const hutang = 2000000
+// const saldoakhir = saldoawal + saldotambahan - hutang
+
+// alert(
+//     nama saya ${nama} usia saya itu ${usia} tinggi saya itu ${tinggibadan}cm berat badan saya ${beratbadan}kg pacar saya ${pacar},
+// )
+// alert(`saldo awal saya sebesar Rp.${saldoawal} dan saldo tambahan saya sebesar Rp.${saldotambahan} jadi total saldo yang saya miliki afalah 
+//    Rp.${saldoakhir}`)
+
+let namaguru = [jodi, budi, andi]
+namaguru.push(april, fadhil)
+namaguru.shift()
+namaguru.pop()
+alert(namaguru)  
